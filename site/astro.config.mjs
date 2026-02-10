93e5dc6fecd3f4a248f185d4612425dcbae2d3e5
Rafael <craice@gmail.com>
2026-02-09 23:05:19 -0300

    Deploy fix.

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://craice.github.io',
+  base: '/socratic',
   vite: {
     plugins: [tailwindcss()]
   }
